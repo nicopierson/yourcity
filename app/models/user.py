b4b3be3d93ae0de5ea856add8aa0fbf57184f13e
Nico Pierson <nicogpt@gmail.com>
2021-08-24 20:01:10 -0700

    finished City model

--- app/models/user.py
+++ app/models/user.py
@@ -11,6 +11,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    city_relation = db.relationship('City', back_populates='user_relation')
+
     @property
     def password(self):
         return self.hashed_password
