bbfcd3025e132660d1ab2b5d53085f517ca2ee25
Nico Pierson <nicogpt@gmail.com>
2021-08-31 16:11:48 -0700

    created footer component and added media queries for profile component

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import { useHistory } from 'react-router-dom';
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const onLogout = async (e) => {
+    e.preventDefault();
     await dispatch(logout());
+    history.push('/');
   };
 
   return <button onClick={onLogout}>Logout</button>;
