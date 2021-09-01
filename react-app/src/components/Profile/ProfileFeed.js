9cca9d44eeef748b8a6f73812b46a714b0a1ed34
Nico Pierson <nicogpt@gmail.com>
2021-08-31 19:07:12 -0700

    included overlay of city for profile page

--- react-app/src/components/Profile/ProfileFeed.js
+++ react-app/src/components/Profile/ProfileFeed.js
@@ -12,91 +12,95 @@ const ProfileFeed = ({ profileId, isOwner }) => {
 
     return (
         <div
-            className={styles.feed_container}
+            className={styles.feed_outer_container}
         >
             <div
-                className={styles.feed_tab_container}
+                className={styles.feed_container}
             >
-                <NavLink 
-                    exact
-                    to={`/profile/${profileId}/cities`}
-                    activeClassName={styles.active}
-                    style={{ textDecoration: 'none' }}
-                    isActive={(match, location) => {
-                      if(location.pathname === `/profile/${profileId}`
-                      || match?.isExact) {
-                          return true;
-                      }
-                      return false;
-                    }}
+                <div
+                    className={styles.feed_tab_container}
                 >
-                    <p>
-                        {isOwner &&
-                            <span>My </span>
-                        } 
-                        Cities
-                    </p>
-                </NavLink>
-                <NavLink 
-                    to={`/profile/${profileId}/insights`}
-                    activeClassName={styles.active}
-                    style={{ textDecoration: 'none' }}
-                >
-                    <p>
-                        {isOwner &&
-                            <span>My </span>
-                        } 
-                        Insights
-                    </p>
-                </NavLink>
-                <NavLink 
-                    to={`/profile/${profileId}/likes`}
-                    activeClassName={styles.active}
-                    style={{ textDecoration: 'none' }}
-                >
-                    <p>Favorites</p>
-                </NavLink>
-                <NavLink 
-                    to={`/profile/${profileId}/matches`}
-                    activeClassName={styles.active}
-                    style={{ textDecoration: 'none' }}
-                >
-                    <p>Matches</p>
-                </NavLink>
-                <NavLink 
-                    to={`/profile/${profileId}/feed`}
-                    activeClassName={styles.active}
-                    style={{ textDecoration: 'none' }}
-                >
-                    <p>Feed</p>
-                </NavLink>
-            </div>
-            <div>
-                <Switch>
-                    <Route exact path={[`/profile/${profileId}/cities`, `/profile/${profileId}/`]}>
-                        <MyCities 
-                            profileId={profileId}
-                        />
-                    </Route>
-                    <Route exact path={`/profile/${profileId}/insights`}>
-                        <MyInsights 
-                            profileId={profileId}
-                        />
-                    </Route>
-                    <Route exact path={`/profile/${profileId}/likes`}>
-                        <MyLikes 
-                            profileId={profileId}
-                        />
-                    </Route>
-                    <Route exact path={`/profile/${profileId}/matches`}>
-                        <MyMatches 
-                            profileId={profileId}
-                        />
-                    </Route>
-                    <Route exact path={`/profile/${profileId}/feed`}>
-                        <MyFeed />
-                    </Route>
-                </Switch>
+                    <NavLink 
+                        exact
+                        to={`/profile/${profileId}/cities`}
+                        activeClassName={styles.active}
+                        style={{ textDecoration: 'none' }}
+                        isActive={(match, location) => {
+                        if(location.pathname === `/profile/${profileId}`
+                        || match?.isExact) {
+                            return true;
+                        }
+                        return false;
+                        }}
+                    >
+                        <p>
+                            {isOwner &&
+                                <span>My </span>
+                            } 
+                            Cities
+                        </p>
+                    </NavLink>
+                    <NavLink 
+                        to={`/profile/${profileId}/insights`}
+                        activeClassName={styles.active}
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <p>
+                            {isOwner &&
+                                <span>My </span>
+                            } 
+                            Insights
+                        </p>
+                    </NavLink>
+                    <NavLink 
+                        to={`/profile/${profileId}/likes`}
+                        activeClassName={styles.active}
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <p>Favorites</p>
+                    </NavLink>
+                    <NavLink 
+                        to={`/profile/${profileId}/matches`}
+                        activeClassName={styles.active}
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <p>Matches</p>
+                    </NavLink>
+                    <NavLink 
+                        to={`/profile/${profileId}/feed`}
+                        activeClassName={styles.active}
+                        style={{ textDecoration: 'none' }}
+                    >
+                        <p>Feed</p>
+                    </NavLink>
+                </div>
+                <div>
+                    <Switch>
+                        <Route exact path={[`/profile/${profileId}/cities`, `/profile/${profileId}/`]}>
+                            <MyCities 
+                                profileId={profileId}
+                            />
+                        </Route>
+                        <Route exact path={`/profile/${profileId}/insights`}>
+                            <MyInsights 
+                                profileId={profileId}
+                            />
+                        </Route>
+                        <Route exact path={`/profile/${profileId}/likes`}>
+                            <MyLikes 
+                                profileId={profileId}
+                            />
+                        </Route>
+                        <Route exact path={`/profile/${profileId}/matches`}>
+                            <MyMatches 
+                                profileId={profileId}
+                            />
+                        </Route>
+                        <Route exact path={`/profile/${profileId}/feed`}>
+                            <MyFeed />
+                        </Route>
+                    </Switch>
+                </div>
             </div>
         </div>
     )
